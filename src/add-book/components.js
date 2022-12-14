d2151a0f403a8e33920439397a9b5e978489f0a2
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2022-12-14 13:33:21 -0500

    add change handlers to add book component

--- src/add-book/components.js
+++ src/add-book/components.js
@@ -2,5 +2,45 @@ import { useSate } from 'react';
 import { Button, Card, FloatingLabel, Form } from 'react-bootstrap';
 
 export function AddBook(props) {
-  
+ 
+  function getBlankBook() {
+    return {
+      title: '',
+      author: '',
+      year: '',
+      image: '',
+      finished: false,
+    }
+  }
+
+  const [book, setBook] = useState(getBlankBook);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.handleAddBook(book);
+    setBook(getBlankBook());
+  } 
+
+  function handleChange(e) {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setBook(oldBook => ({
+      ...oldBook,
+      [name]: value
+    }));
+  }
+
+  return (
+    <section className="input_section">
+      <Card>
+        <Card.Header>Add a new Book</Card.Header>
+        <Card.Body className='card-body'>
+          <Form onSubmit={handleSubmit}>
+            
+          </Form>
+        </Card.Body>
+      </Card>
+    </section>
+  );
 }
